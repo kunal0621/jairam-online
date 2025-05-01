aaa03147406397c00313df982dd8499a449e780b
kunal singh <kunalsinghksr0021@gmail.com>
2025-05-01 10:35:56 +0530

    broweser to hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { AuthProvider } from "@/context/AuthContext";
 import { QueryProvider } from "@/lib/react-query/QueryProvider";
@@ -9,12 +9,12 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </QueryProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
